300ecbe61735ff43ca0c2e34f7dd0b4b1ca493b5
taleaa836-pia <taleaa836@gmail.com>
2026-03-05 10:46:24 +0700

    fixing error

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,12 +1,20 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "sopia_db";
+$host = getenv('DB_HOST') ?: "localhost";
+$user = getenv('DB_USER') ?: "root";
+$pass = getenv('DB_PASSWORD') ?: "";
+$db   = getenv('DB_NAME') ?: "sopia_db";
+$port = getenv('DB_PORT') ?: "3306";
+$ssl_ca = getenv('DB_SSL_CA'); // Jalur opsional ke SSL CA untuk Aiven
 
-$koneksi = mysqli_connect($host, $user, $pass, $db);
+$koneksi = mysqli_init();
 
-if (!$koneksi) {
+if ($ssl_ca) {
+    mysqli_ssl_set($koneksi, NULL, NULL, $ssl_ca, NULL, NULL);
+}
+
+// Koneksi dengan port
+if (!mysqli_real_connect($koneksi, $host, $user, $pass, $db, $port)) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
 ?>
+
