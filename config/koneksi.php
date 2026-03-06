c767e2f192245d546fd15b68f29406d07f3e61f2
taleaa836-pia <taleaa836@gmail.com>
2026-03-06 07:36:29 +0700

    fixing error

--- config/koneksi.php
+++ config/koneksi.php
@@ -8,8 +8,19 @@ $ssl_ca = getenv('DB_SSL_CA'); // Jalur opsional ke SSL CA untuk Aiven
 
 $koneksi = mysqli_init();
 
-if ($ssl_ca) {
-    mysqli_ssl_set($koneksi, NULL, NULL, $ssl_ca, NULL, NULL);
+if ($ssl_ca && $ssl_ca !== 'REQUIRED') {
+    // Check if DB_SSL_CA environment variable contains the certificate content or a path
+    if (strpos($ssl_ca, '-----BEGIN CERTIFICATE-----') !== false) {
+        // If certificate content, write it to a temporary file
+        $temp_ca = tempnam(sys_get_temp_dir(), 'ca_');
+        file_put_contents($temp_ca, $ssl_ca);
+        $ssl_ca = $temp_ca;
+    }
+    
+    // Set SSL if we have a path (either a temp file or a direct path provided in DB_SSL_CA)
+    if (file_exists($ssl_ca)) {
+        mysqli_ssl_set($koneksi, NULL, NULL, $ssl_ca, NULL, NULL);
+    }
 }
 
 // Koneksi dengan port
